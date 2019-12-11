df7dc09edc0a7afa3e7e4c8def8c9a6e43c354c5
Simon EL Mansoury <saymon3228@gmail.com>
2019-12-11 15:40:05 +0200

    Modifications

---
+++ src/components/info.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Info = () => (<div className = "info">
+  <h2 >Weather Application</h2>
+  </div>);
+
+export default Info;
---
+++ src/components/weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Weather = props => (  <div>
+  { props.city &&
+    <div className = "infoWeath">
+      <p>Location: {props.city}, {props.country}</p>
+      <p>Tempeture: {props.temp } C° </p>
+      <p>Pressure: {props.pressure} Pa</p>
+      <p>Sunset: {props.sunset}</p>
+    </div>
+  }
+  <p className = "error">{ props.error}</p>
+  </div>);
+export default Weather;
